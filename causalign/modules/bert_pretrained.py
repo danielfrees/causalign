6a91706e3a49418539f458ee23c421b801d9e41a
Martin Pollack <Martin.Pollack@ibm.com>
2024-11-04 22:03:47 -0500

    add baseline

--- causalign/modules/bert_pretrained.py
+++ causalign/modules/bert_pretrained.py
@@ -1,4 +1,17 @@
-""" 
-Implementation of our pre-trained BERT model. OG Bansal paper used: 'msmarco-distilbert-base-v3'
-"""
+from torch import nn
+from transformers import BertModel
 
+class BertPreTrained(nn.Module):
+    """
+    BERT embedding model for extracting [CLS] token embeddings.
+    """
+    def __init__(self):
+        super(BertPreTrained, self).__init__()
+        self.bert = BertModel.from_pretrained('bert-base-uncased')
+        for param in self.bert.parameters():
+            param.requires_grad = True
+
+    def forward(self, input_ids, attention_mask):
+        bert_output = self.bert(input_ids, attention_mask)
+        pooler_output = bert_output['pooler_output']
+        return pooler_output
---
+++ causalign/modules/finetuned_baseline.py
@@ -0,0 +1,110 @@
+from types import SimpleNamespace
+
+import torch
+from torch import nn
+from torch.optim import AdamW
+from torch.utils.data import DataLoader, Subset
+from torch.amp import autocast, GradScaler
+from tqdm import tqdm
+from datasets import (
+    NLIClassificationDataset,
+    load_nli_data
+)
+
+from causalign.modules.bert_pretrained import BertPreTrained
+from causalign.modules.utils import save_model, get_training_args, seed_everything
+
+TQDM_DISABLE = False
+BERT_HIDDEN_SIZE = 768
+
+def main(args):
+    """
+    Main function to run the training loop.
+    """
+    device = torch.device('cuda') if args.use_gpu else torch.device('cpu')
+
+    nli_data = load_nli_data(args.nli_filename)
+    print("NLI Data loaded")
+    dataset = NLIClassificationDataset(nli_data, args)
+    dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True, collate_fn=dataset.collate_fn,
+                            num_workers=args.num_workers)
+    print("NLI Dataloader created")
+
+    # Setup config to be saved later
+    config = {'tau': args.tau,
+              'hidden_size': BERT_HIDDEN_SIZE,
+              'data_dir': '.'}
+    config = SimpleNamespace(**config)
+
+    model = BertPreTrained().to(device)
+    print("Model initialized")
+    criterion = ContrastiveLearningLoss(tau=args.tau)
+
+    optimizer = AdamW(model.parameters(), lr=args.lr)
+    scaler = GradScaler('cuda')
+
+
+    for epoch in range(args.epochs):
+        model.train()
+        running_loss = 0.0
+
+        for batch in tqdm(dataloader):
+            token_ids_1 = batch['token_ids_1'].to(device)
+            attention_mask_1 = batch['attention_mask_1'].to(device)
+            token_ids_2 = batch['token_ids_2'].to(device)
+            attention_mask_2 = batch['attention_mask_2'].to(device)
+            token_ids_3 = batch['token_ids_3'].to(device)
+            attention_mask_3 = batch['attention_mask_3'].to(device)
+
+            optimizer.zero_grad()
+
+            with autocast():
+                premise_emb = model(token_ids_1, attention_mask_1)
+                entailment_emb = model(token_ids_2, attention_mask_2)
+                contradiction_emb = model(token_ids_3, attention_mask_3)
+
+                loss = criterion(premise_emb, entailment_emb, contradiction_emb)
+
+            scaler.scale(loss).backward()
+            scaler.step(optimizer)
+            scaler.update()
+
+            running_loss += loss.item()
+
+        print(f"Epoch {epoch + 1}, Loss: {running_loss / len(dataloader)}")
+
+    # Save the trained model
+    save_model(model, optimizer, args, config, args.output_model_path)
+    print(f"Model saved to {args.output_model_path}")
+
+class ContrastiveLearningLoss(nn.Module):
+    def __init__(self, tau):
+        super(ContrastiveLearningLoss, self).__init__()
+        self.tau = tau
+        self.cosine_similarity = nn.CosineSimilarity(dim=1, eps=1e-6)
+
+    def forward(self, premise_emb, entailment_emb, contradiction_emb):
+        """
+        premise_emb: tensor of size (batch_size, embedding_size)
+        entailment_emb: tensor of size (batch_size, embedding_size)
+        contradiction_emb: tensor of size (batch_size, embedding_size)
+        """
+
+        cos_sim_pos = self.cosine_similarity(premise_emb, entailment_emb) # ()
+        cos_sim_neg = self.cosine_similarity(premise_emb, contradiction_emb)
+
+        scaled_logit_pos = torch.exp(torch.div(cos_sim_pos, self.tau))
+        scaled_logit_neg = torch.exp(torch.div(cos_sim_neg, self.tau))
+
+        numerator = scaled_logit_pos
+        denominator = scaled_logit_pos.sum(dim=0) + scaled_logit_neg.sum(dim=0)
+
+        output = torch.mul(torch.log(torch.div(numerator, denominator)), -1)
+        return output.mean()
+
+
+if __name__ == '__main__':
+    args = get_training_args()
+    args.output_model_path = f'params-{args.epochs}-{args.lr}-{args.tau}-contrastive-baseline.pt'  # Save path.
+    seed_everything(args.seed)
+    main(args)
